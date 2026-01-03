ea5e5b0a9cab257d7e00202e4a07aa7040b28f8e
Ahmad Ghaddar <ahmadsonji019@gmail.com>
2026-01-03 18:44:43 +0200

    db deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,16 +3,13 @@ dotenv.config();
 import mysql from 'mysql2';
 
 
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+export const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10, 
+  queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error(err.code + err.message);
-    }
-});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -94,4 +94,5 @@ router.post("/login", (req, res) => {
 });
 
 
+
 export default router;
